782f922af654a3a555eb79826785d2ac25fdf9e3
suvesh <suveshgurung2@gmail.com>
2024-07-27 19:38:07 +0545

    enabled raw mode with canonical mode

--- macho.c
+++ macho.c
@@ -1,7 +1,31 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+struct termios origTermios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermios);
+}
+
+void enableRawMode() {
+
+    tcgetattr(STDIN_FILENO, &origTermios);
+    atexit(disableRawMode);
+
+    struct termios raw = origTermios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
 
-    printf("Hello\n");
-    return 1;
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+    return 0;
 }
