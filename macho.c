6b0841fd2a7efabfbf95adae4a01c9f0cef5d770
suvesh <suveshgurung2@gmail.com>
2024-07-28 16:46:55 +0545

    raw mode settings done (i guess)

--- macho.c
+++ macho.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -11,11 +13,16 @@ void disableRawMode() {
 
 void enableRawMode() {
 
+    // get current terminal attributes.
     tcgetattr(STDIN_FILENO, &origTermios);
+    // call disableRawMode when program exits.
     atexit(disableRawMode);
 
     struct termios raw = origTermios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -25,7 +32,13 @@ int main() {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
 
     return 0;
 }
