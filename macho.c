28e35c7ede3fad397af2a7d7c5b7480425bf41d4
suvesh <suveshgurung2@gmail.com>
2024-09-25 20:55:50 +0545

    allow user to press right or left key to move to adjacent line

--- macho.c
+++ macho.c
@@ -383,11 +383,17 @@ void moveEditorCursor(int key) {
         case ARROW_LEFT:
             if (E.cx != 0) {
                 E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
+            } else if (row && E.cy < E.numRows) {
+                E.cy++;
+                E.cx = 0;
             }
             break;
         case ARROW_UP:
